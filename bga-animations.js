16705d030e7dbdd4fd48865de99a1dcbb275a356
Jay Sachs <jaysachs@users.noreply.github.com>
2024-12-09 20:14:36 -0800

    Move screen-center animation to use standard scaffolding

--- bga-animations.js
+++ bga-animations.js
@@ -284,51 +284,19 @@ var BgaShowScreenCenterAnimation = /** @class */ (function (_super) {
     BgaShowScreenCenterAnimation.prototype.doAnimate = function (animationManager) {
         var _this = this;
         var promise = new Promise(function (success) {
-            var _a, _b, _c, _d;
-            var settings = _this.settings;
-            var element = settings.element;
+            var _a, _b, _c, _d, _e, _f, _g, _h;
+            var element = _this.settings.element;
             var elementBR = animationManager.game.getBoundingClientRectIgnoreZoom(element);
             var xCenter = (elementBR.left + elementBR.right) / 2;
             var yCenter = (elementBR.top + elementBR.bottom) / 2;
             var x = xCenter - (window.innerWidth / 2);
             var y = yCenter - (window.innerHeight / 2);
-            var duration = (_a = settings === null || settings === void 0 ? void 0 : settings.duration) !== null && _a !== void 0 ? _a : 500;
-            console.log("duration in center show is ", duration);
-            var originalZIndex = element.style.zIndex;
-            var originalTransition = element.style.transition;
-            var transitionTimingFunction = (_b = settings.transitionTimingFunction) !== null && _b !== void 0 ? _b : 'linear';
-            element.style.zIndex = "".concat((_c = settings === null || settings === void 0 ? void 0 : settings.zIndex) !== null && _c !== void 0 ? _c : 10);
-            var timeoutId = null;
-            var cleanOnTransitionEnd = function () {
-                console.log("cleanOnEnd", _this);
-                element.style.zIndex = originalZIndex;
-                element.style.transition = originalTransition;
-                success();
-                element.removeEventListener('transitioncancel', cleanOnTransitionEnd);
-                element.removeEventListener('transitionend', cleanOnTransitionEnd);
-                document.removeEventListener('visibilitychange', cleanOnTransitionEnd);
-                if (timeoutId) {
-                    clearTimeout(timeoutId);
-                }
-            };
-            var cleanOnTransitionCancel = function () {
-                var _a;
-                console.log("cleanOnCancel", _this);
-                element.style.transition = "";
-                element.offsetHeight;
-                element.style.transform = (_a = settings === null || settings === void 0 ? void 0 : settings.finalTransform) !== null && _a !== void 0 ? _a : null;
-                element.offsetHeight;
-                cleanOnTransitionEnd();
-            };
-            element.addEventListener('transitioncancel', cleanOnTransitionEnd);
-            element.addEventListener('transitionend', cleanOnTransitionEnd);
-            document.addEventListener('visibilitychange', cleanOnTransitionCancel);
-            element.offsetHeight;
+            var transitionTimingFunction = (_a = _this.settings.transitionTimingFunction) !== null && _a !== void 0 ? _a : 'linear';
+            var duration = (_c = (_b = _this.settings) === null || _b === void 0 ? void 0 : _b.duration) !== null && _c !== void 0 ? _c : 500;
+            _this.wireUp(element, duration, success);
             element.style.transition = "transform ".concat(duration, "ms ").concat(transitionTimingFunction);
-            element.offsetHeight;
-            element.style.transform = "translate(".concat(-x, "px, ").concat(-y, "px) rotate(").concat((_d = settings === null || settings === void 0 ? void 0 : settings.rotationDelta) !== null && _d !== void 0 ? _d : 0, "deg)");
-            // safety in case transitionend and transitioncancel are not called
-            timeoutId = setTimeout(cleanOnTransitionEnd, duration + 100);
+            element.style.zIndex = "".concat((_e = (_d = _this.settings) === null || _d === void 0 ? void 0 : _d.zIndex) !== null && _e !== void 0 ? _e : 10);
+            element.style.transform = "translate(".concat(-x, "px, ").concat(-y, "px) rotate(").concat((_g = (_f = _this.settings) === null || _f === void 0 ? void 0 : _f.rotationDelta) !== null && _g !== void 0 ? _g : 0, "deg) scale(").concat((_h = _this.settings.scale) !== null && _h !== void 0 ? _h : 1, ")");
         });
         return promise;
     };
--- src/animations/show-screen-center-animation.ts
+++ src/animations/show-screen-center-animation.ts
@@ -17,9 +17,8 @@ class BgaShowScreenCenterAnimation<BgaAnimation> extends BgaAnimation<any> {
 
     protected doAnimate(animationManager: AnimationManager): Promise<void> {
         const promise = new Promise<void>((success) => {
-            const settings = this.settings;
-            const element = settings.element;
-    
+            const element = this.settings.element;
+
             const elementBR = animationManager.game.getBoundingClientRectIgnoreZoom(element);
     
             const xCenter = (elementBR.left + elementBR.right)/2;
@@ -27,49 +26,15 @@ class BgaShowScreenCenterAnimation<BgaAnimation> extends BgaAnimation<any> {
     
             const x = xCenter - (window.innerWidth / 2);
             const y = yCenter - (window.innerHeight / 2);
-    
-            const duration = settings?.duration ?? 500;
-            console.log("duration in center show is ", duration);
-            const originalZIndex = element.style.zIndex;
-            const originalTransition = element.style.transition;
-            const transitionTimingFunction = settings.transitionTimingFunction ?? 'linear';
-    
-            element.style.zIndex = `${settings?.zIndex ?? 10}`;
-    
-            let timeoutId = null;
-    
-            const cleanOnTransitionEnd = () => {
-                console.log("cleanOnEnd", this);
-                element.style.zIndex = originalZIndex;
-                element.style.transition = originalTransition;
-                success();
-                element.removeEventListener('transitioncancel', cleanOnTransitionEnd);
-                element.removeEventListener('transitionend', cleanOnTransitionEnd);
-                document.removeEventListener('visibilitychange', cleanOnTransitionEnd);
-                if (timeoutId) {
-                    clearTimeout(timeoutId);
-                }
-            };
-    
-            const cleanOnTransitionCancel = () => {
-                console.log("cleanOnCancel", this);
-                element.style.transition = ``;
-                element.offsetHeight;
-                element.style.transform = settings?.finalTransform ?? null;
-                element.offsetHeight;
-                cleanOnTransitionEnd();
-            }
-    
-            element.addEventListener('transitioncancel', cleanOnTransitionEnd);
-            element.addEventListener('transitionend', cleanOnTransitionEnd);
-            document.addEventListener('visibilitychange', cleanOnTransitionCancel);
-    
-            element.offsetHeight;
+
+            const transitionTimingFunction = this.settings.transitionTimingFunction ?? 'linear';
+            const duration = this.settings?.duration ?? 500;
+
+            this.wireUp(element, duration, success);
+
             element.style.transition = `transform ${duration}ms ${transitionTimingFunction}`;
-            element.offsetHeight;
-            element.style.transform = `translate(${-x}px, ${-y}px) rotate(${settings?.rotationDelta ?? 0}deg)`;
-            // safety in case transitionend and transitioncancel are not called
-            timeoutId = setTimeout(cleanOnTransitionEnd, duration + 100);
+            element.style.zIndex = `${this.settings?.zIndex ?? 10}`;    
+            element.style.transform = `translate(${-x}px, ${-y}px) rotate(${this.settings?.rotationDelta ?? 0}deg) scale(${this.settings.scale ?? 1})`;
         });
         return promise;
     }
