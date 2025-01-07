1274e390f9d5ae0cdbcd57dc2e4b65c5a0a7e953
Jay Sachs <jaysachs@users.noreply.github.com>
2025-01-06 18:49:56 -0800

    Allow specifying iterations in fade animation

--- src/animations/fade-animation.ts
+++ src/animations/fade-animation.ts
@@ -4,6 +4,7 @@
 
 interface BgaFadeAnimationSettings extends BgaElementAnimationSettings {
    kind: "in" | "out" | "outin";
+   iterations?: number;
 }
 
 class BgaFadeAnimation<T extends BgaFadeAnimationSettings> extends BgaElementAnimation<T> {
@@ -24,27 +25,23 @@ class BgaFadeAnimation<T extends BgaFadeAnimationSettings> extends BgaElementAni
             // element.style.zIndex = `${this.settings?.zIndex ?? 10}`;
             let frames = [];
             switch      (this.settings.kind) {
-            case "in":  frames.push({ opacity: 1 }); break;
-            case "out": frames.push({ opacity: 0 }); break;
+            case "in":  frames.push({ opacity: 0 }, { opacity: 1 }); break;
+            case "out": frames.push({ opacity: 1 }, { opacity: 0 }); break;
             case "outin": frames.push({opacity: 1}, { opacity: 0 }, { opacity: 1 }); break;
             }
-//            var direction: "reverse" | "normal" | "alternate"  = "normal";
-//            var iterations = 1;
-//            if (this.settings.kind == "in") { direction = "reverse"; }
-//            else if (this.settings.kind == "outin") { direction = "alternate"; iterations = 2 }
             let a = new Animation(
-            new KeyframeEffect(element,
-            frames,
-             {
-               duration: duration,
-  //             easing: this.settings.transitionTimingFunction ?? 'linear',
-//               direction: direction,
-               fill: "forwards",
-               iterations: 1,
-             }));
+              new KeyframeEffect(
+                element,
+                frames,
+                {
+                  duration: duration,
+                  easing: this.settings.transitionTimingFunction ?? 'linear',
+                  fill: "forwards",
+                  iterations: this.settings.iterations ?? 1,
+                }));
              a.onfinish = e => {
               a.commitStyles();
-             //    element.style.transform = this.settings?.finalTransform ?? null;
+             // element.style.transform = this.settings?.finalTransform ?? null;
              };
              a.play();
         });
